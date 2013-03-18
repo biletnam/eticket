270343abf3e8625549678223ffe6231560c15a18
Stefen Gratz <thanhle@htmlfivemedia.com>
2013-03-18 15:41:25 +0700

    Sửa lỗi, cập nhật manage order, order details

---
+++ front/protected/views/user/order-detail.php
@@ -0,0 +1,27 @@
+<section class="manage-event">
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th width="220px">TICKET</th>
+                <th width="220px">EVENT</th>
+                <th>QUANTITY</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($ticket_paids as $t) :?>
+            <tr>
+                <td><a href="#"><?php echo $t['ticket_type_name']?></a></td>
+                <td class="text-bold"><a href="<?php echo HelperUrl::baseUrl()?>event/info/s/<?php echo $t['event_slug']?>"><?php echo $t['event_name']?></a></td>
+                <td><?php echo $t['total_ticket']?></td>
+                <td>
+                    <div>
+                        <a class="btn-style btn-primary" href="<?php echo HelperUrl::baseUrl()?>user/ticket/<?php echo $t['ticket_type_id']?>">Detail</a>
+                    </div>
+                </td>
+            </tr>
+            <?php endforeach;?>
+            
+        </tbody>
+    </table>
+</section>

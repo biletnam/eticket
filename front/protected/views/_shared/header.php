fd9c62eccd079f15cb862eb3e5a92457651c6a6f
Stefen Gratz <thanhle@htmlfivemedia.com>
2013-04-08 11:30:56 +0700

    Cap nhat v ersion CSS

--- front/protected/views/_shared/header.php
+++ front/protected/views/_shared/header.php
@@ -15,8 +15,8 @@
                             <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/reset.css" />
                             <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/text.css" />
                             <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/960.css" />
-                            <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/style.css" />
-                            <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/custom.css" />
+                            <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/style.css?v=08042013" />
+                            <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/custom.css?v=08042013" />
                             <link href="<?php echo HelperUrl::baseUrl(); ?>/css/themes/start/jquery.ui.all.css" rel="stylesheet"/>
                             <link rel="stylesheet" href="<?php echo HelperUrl::baseUrl(); ?>css/flex_slider.css" type="text/css" media="screen" />
                             <link rel="stylesheet" type="text/css" href="<?php echo HelperUrl::baseUrl(); ?>js/fancybox/jquery.fancybox.css" />
